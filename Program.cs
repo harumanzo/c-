02ece0e48c4e7cb80db373f756d1dce5d5fb5959
harumanzo <shuaige0028@gmail.com>
2024-09-23 23:53:57 +0900

    조건문

--- Program.cs
+++ Program.cs
@@ -2,16 +2,28 @@
 using System;
 class MainClass {
     public static void Main(string[] args){
-        //변수
-        bool boolVariable = true;
-        int intVariablae = 10;
-        float floatVariable = 3.4f;
-        char charVariable = 'a';
-        Console.WriteLine(boolVariable);
-        Console.WriteLine(intVariablae);
-        Console.WriteLine(floatVariable);
-        Console.WriteLine(charVariable);
+        // 
+        int num = 0;
+        if (num > 0)
+        {
+            Console.WriteLine("양수");
+        }
+        else if (num < 0) {
+            Console.WriteLine("음수");
+        } else {
+            Console.WriteLine("제로");
+        }
     }
     
 }
 
+/*변수
+bool boolVariable = true;
+int intVariablae = 10;
+float floatVariable = 3.4f;
+char charVariable = 'a';
+Console.WriteLine(boolVariable);
+Console.WriteLine(intVariablae);
+Console.WriteLine(floatVariable);
+Console.WriteLine(charVariable);
+*/
