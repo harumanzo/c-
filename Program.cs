1273b4f78d0a597b7523770dd7f3588432db4041
harumanzo <shuaige0028@gmail.com>
2024-09-23 23:50:22 +0900

    변수

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class MainClass {
+    public static void Main(string[] args){
+        //변수
+        bool boolVariable = true;
+        int intVariablae = 10;
+        float floatVariable = 3.4f;
+        char charVariable = 'a';
+        Console.WriteLine(boolVariable);
+        Console.WriteLine(intVariablae);
+        Console.WriteLine(floatVariable);
+        Console.WriteLine(charVariable);
+    }
+    
+}
+
