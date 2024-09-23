1394c02ac2b63d4cb6dd2885693d2b2107622d03
harumanzo <shuaige0028@gmail.com>
2024-09-24 00:07:06 +0900

    반복문

--- Program.cs
+++ Program.cs
@@ -1,8 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 using System;
 class MainClass {
-    public static void Main(string[] args){
-        // 
+    public static void Main(string[] args)
+    {
+        //반복문 for(초기화;조건식;반복식){반복코드}
+        for (int i = 1; i < 11; i++) {
+            Console.WriteLine(i);
+        } 
+
+    }
+  
+}
+
+/*
+ 변수
+bool boolVariable = true;
+int intVariablae = 10;
+float floatVariable = 3.4f;
+char charVariable = 'a';
+Console.WriteLine(boolVariable);
+Console.WriteLine(intVariablae);
+Console.WriteLine(floatVariable);
+Console.WriteLine(charVariable);
+
+조건문
+if(조건식){}
+else if(조건식){}
+else (조건식){}
         int num = 0;
         if (num > 0)
         {
@@ -13,17 +37,4 @@ class MainClass {
         } else {
             Console.WriteLine("제로");
         }
-    }
-    
-}
-
-/*변수
-bool boolVariable = true;
-int intVariablae = 10;
-float floatVariable = 3.4f;
-char charVariable = 'a';
-Console.WriteLine(boolVariable);
-Console.WriteLine(intVariablae);
-Console.WriteLine(floatVariable);
-Console.WriteLine(charVariable);
 */
