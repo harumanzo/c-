1e9f376687d78a6c067418fdd2d4a9c58dc4ef25
harumanzo <shuaige0028@gmail.com>
2024-09-24 23:09:52 +0900

    주석처리 및 마무리

--- calculator/calculator/Form1.cs
+++ calculator/calculator/Form1.cs
@@ -114,21 +114,25 @@ namespace calculator
         private void buttonPlus_Click(object sender, EventArgs e)
         {
             
-            if (currentOperator == 0)
+            if (currentOperator == 0)/* 부호를 쓰지않은 상태이거나 =으로 계산 마무리했을 상태에서 구현*/
             {
                 firstOperand = Int32.Parse(display.Text);
                 display.Text = "";
                 operatorChangeFlag = true;
                 currentOperator = Operators.Add;
             }
-            else {
-                if (currentOperator == Operators.Add) {
-                    secondOperand = Int32.Parse(display.Text);
-                    firstOperand += secondOperand;
-                    secondOperand = 0;
+            else
+            {/* =을 쓰지않고 연속계산할때 아래와 같이 구현함*/
+                if (currentOperator == Operators.Add)
+                {/* 이전 부호가 +였을경우 */
+                    secondOperand = Int32.Parse(display.Text);//전회차에 적었던 숫자를 저장
+                    firstOperand += secondOperand; //전전숫자와 계산해주고
+                    secondOperand = 0;//변수를 초기화
                     display.Text = "";
-                    currentOperator = Operators.Add;
-                }else if(currentOperator == Operators.Subtract) {
+                    currentOperator = Operators.Add;/* 마무리는 클릭했던 +버튼으로 currentOperator을 설정해준다 =을 누를경우를 생각해야하기때문*/
+                }
+                else if(currentOperator == Operators.Subtract)
+                {/* 이전 부호가 -였을경우 */
                     secondOperand = Int32.Parse(display.Text);
                     firstOperand -= secondOperand;
                     secondOperand = 0;
@@ -136,14 +140,14 @@ namespace calculator
                     currentOperator = Operators.Add;
                 }
                 else if(currentOperator == Operators.Multiply)
-                {
+                {/* 이전 부호가 *였을경우 */
                     secondOperand = Int32.Parse(display.Text);
                     firstOperand *= secondOperand;
                     secondOperand = 0;
                     display.Text = "";
                     currentOperator = Operators.Add;
                 }else if(currentOperator == Operators.Divide)
-                {
+                {/* 이전 부호가 /였을경우 */
                     secondOperand = Int32.Parse(display.Text);
                     firstOperand /= secondOperand;
                     secondOperand = 0;
