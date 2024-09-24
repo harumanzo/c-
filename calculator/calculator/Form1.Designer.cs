be13c6402ff3a65ee5d5ca480d2224a65c7447a1
harumanzo <shuaige0028@gmail.com>
2024-09-24 21:20:33 +0900

    정수 두개 사칙연산

--- calculator/calculator/Form1.Designer.cs
+++ calculator/calculator/Form1.Designer.cs
@@ -28,11 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            diaplay = new Label();
+            display = new Label();
             tableLayoutPanel1 = new TableLayoutPanel();
             button16 = new Button();
             buttonFloat = new Button();
-            button0 = new Button();
             buttonAc = new Button();
             button12 = new Button();
             button11 = new Button();
@@ -43,23 +42,24 @@
             button4 = new Button();
             button5 = new Button();
             button6 = new Button();
-            button7 = new Button();
             button8 = new Button();
             button9 = new Button();
+            button0 = new Button();
+            button7 = new Button();
             buttonEqual = new Button();
             tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
-            // diaplay
+            // display
             // 
-            diaplay.Dock = DockStyle.Top;
-            diaplay.Font = new Font("맑은 고딕", 24F, FontStyle.Regular, GraphicsUnit.Point);
-            diaplay.Location = new Point(0, 0);
-            diaplay.Name = "diaplay";
-            diaplay.Size = new Size(800, 70);
-            diaplay.TabIndex = 0;
-            diaplay.Text = "0";
-            diaplay.TextAlign = ContentAlignment.MiddleRight;
+            display.Dock = DockStyle.Top;
+            display.Font = new Font("맑은 고딕", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            display.Location = new Point(0, 0);
+            display.Name = "display";
+            display.Size = new Size(800, 70);
+            display.TabIndex = 0;
+            display.Text = "0";
+            display.TextAlign = ContentAlignment.MiddleRight;
             // 
             // tableLayoutPanel1
             // 
@@ -103,6 +103,7 @@
             button16.TabIndex = 15;
             button16.Text = "÷";
             button16.UseVisualStyleBackColor = true;
+            button16.Click += button16_Click;
             // 
             // buttonFloat
             // 
@@ -113,16 +114,7 @@
             buttonFloat.TabIndex = 14;
             buttonFloat.Text = ".";
             buttonFloat.UseVisualStyleBackColor = true;
-            // 
-            // button0
-            // 
-            button0.Font = new Font("맑은 고딕", 24F, FontStyle.Bold, GraphicsUnit.Point);
-            button0.Location = new Point(203, 243);
-            button0.Name = "button0";
-            button0.Size = new Size(194, 70);
-            button0.TabIndex = 13;
-            button0.Text = "0";
-            button0.UseVisualStyleBackColor = true;
+            buttonFloat.Click += buttonFloat_Click;
             // 
             // buttonAc
             // 
@@ -133,6 +125,7 @@
             buttonAc.TabIndex = 12;
             buttonAc.Text = "Ac";
             buttonAc.UseVisualStyleBackColor = true;
+            buttonAc.Click += buttonAc_Click;
             // 
             // button12
             // 
@@ -143,6 +136,7 @@
             button12.TabIndex = 11;
             button12.Text = "×";
             button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
             // 
             // button11
             // 
@@ -153,6 +147,7 @@
             button11.TabIndex = 10;
             button11.Text = "-";
             button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
             // 
             // buttonPlus
             // 
@@ -163,6 +158,7 @@
             buttonPlus.TabIndex = 9;
             buttonPlus.Text = "+";
             buttonPlus.UseVisualStyleBackColor = true;
+            buttonPlus.Click += buttonPlus_Click;
             // 
             // button3
             // 
@@ -173,6 +169,7 @@
             button3.TabIndex = 2;
             button3.Text = "3";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // button2
             // 
@@ -205,6 +202,7 @@
             button4.TabIndex = 3;
             button4.Text = "4";
             button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // button5
             // 
@@ -215,6 +213,7 @@
             button5.TabIndex = 4;
             button5.Text = "5";
             button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
             // 
             // button6
             // 
@@ -225,16 +224,7 @@
             button6.TabIndex = 5;
             button6.Text = "6";
             button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            button7.Font = new Font("맑은 고딕", 24F, FontStyle.Bold, GraphicsUnit.Point);
-            button7.Location = new Point(3, 163);
-            button7.Name = "button7";
-            button7.Size = new Size(194, 70);
-            button7.TabIndex = 6;
-            button7.Text = "7";
-            button7.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
             // 
             // button8
             // 
@@ -245,6 +235,7 @@
             button8.TabIndex = 7;
             button8.Text = "8";
             button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
             // 
             // button9
             // 
@@ -255,6 +246,29 @@
             button9.TabIndex = 8;
             button9.Text = "9";
             button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button0
+            // 
+            button0.Font = new Font("맑은 고딕", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            button0.Location = new Point(203, 243);
+            button0.Name = "button0";
+            button0.Size = new Size(194, 70);
+            button0.TabIndex = 13;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = true;
+            button0.Click += button0_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("맑은 고딕", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            button7.Location = new Point(3, 163);
+            button7.Name = "button7";
+            button7.Size = new Size(194, 70);
+            button7.TabIndex = 6;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
             // 
             // buttonEqual
             // 
@@ -265,6 +279,7 @@
             buttonEqual.TabIndex = 2;
             buttonEqual.Text = "=";
             buttonEqual.UseVisualStyleBackColor = true;
+            buttonEqual.Click += buttonEqual_Click;
             // 
             // Form1
             // 
@@ -273,7 +288,7 @@
             ClientSize = new Size(800, 490);
             Controls.Add(buttonEqual);
             Controls.Add(tableLayoutPanel1);
-            Controls.Add(diaplay);
+            Controls.Add(display);
             Name = "Form1";
             Text = "Form1";
             tableLayoutPanel1.ResumeLayout(false);
@@ -282,7 +297,7 @@
 
         #endregion
 
-        private Label diaplay;
+        private Label display;
         private TableLayoutPanel tableLayoutPanel1;
         private Button button16;
         private Button buttonFloat;
--- calculator/calculator/Form1.cs
+++ calculator/calculator/Form1.cs
@@ -1,21 +1,164 @@
+
+
 namespace calculator
 {
+    
+
+
     public partial class Form1 : Form
     {
+        enum Operators
+        {
+            None,
+            Add,
+            Subtract,
+            Multiply,
+            Divide,
+            Result
+        }
+        Operators currentOperator = Operators.None;
+        Boolean operatorChangeFlag = false;
+        int firstOperand = 0;
+        int secondOperand = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            if (operatorChangeFlag == true)
+            {
+                display.Text = "";
+                operatorChangeFlag = false;
+            }
+            string strNumber = display.Text += "1";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
         }
         private void button2_Click(object sender, EventArgs e)
         {
+            string strNumber = display.Text += "2";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string strNumber = display.Text += "3";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string strNumber = display.Text += "4";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string strNumber = display.Text += "5";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string strNumber = display.Text += "6";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string strNumber = display.Text += "7";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string strNumber = display.Text += "8";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string strNumber = display.Text += "9";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            string strNumber = display.Text += "0";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void buttonFloat_Click(object sender, EventArgs e)
+        {
+            string strNumber = display.Text += "0";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            firstOperand = Int32.Parse(display.Text);
+            display.Text = "";
+            operatorChangeFlag = true;
+            currentOperator = Operators.Add;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            firstOperand = Int32.Parse(display.Text);
+            display.Text = "";
+            operatorChangeFlag = true;
+            currentOperator = Operators.Subtract;
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            firstOperand = Int32.Parse(display.Text);
+            display.Text = "";
+            operatorChangeFlag = true;
+            currentOperator = Operators.Multiply;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            firstOperand = Int32.Parse(display.Text);
+            display.Text = "";
+            operatorChangeFlag = true;
+            currentOperator = Operators.Divide;
+        }
+
+        private void buttonAc_Click(object sender, EventArgs e)
+        {
+            display.Text = "0";
+            operatorChangeFlag = false;
+            currentOperator = Operators.None;
+            firstOperand = 0;
+            secondOperand = 0;
+        }
+
+        private void buttonEqual_Click(object sender, EventArgs e)
+        {
+            secondOperand = Int32.Parse(display.Text);
+            if(currentOperator == Operators.Add)
+            {
+                int sum = secondOperand + firstOperand;
+                display.Text = sum.ToString();
+            }
         }
     }
 }
