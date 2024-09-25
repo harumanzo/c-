7d33d6ccaf290af16fdb15bf24678378606d2d30
harumanzo <shuaige0028@gmail.com>
2024-09-25 10:22:06 +0900

    함수

---
+++ variable/variable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+//파이어볼사용->mp 5 사용 후 파이어볼 발사
+//->함수 사용하면 위를 100번적는  비효율적 과정 안거쳐도됨
+
+//void FireBall() { }
+
+class Monster {
+
+}
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        
+
+
+
+    }
+
+}
